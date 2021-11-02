9ad6ee2c7dc3a3c591cf7b76d49b8a44dcd666b5
Elisa Bavaresco <elisabavaresco@Elisas-MacBook-Air.local>
2021-11-02 14:47:38 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: const Text('Transferências'),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    ),
+  ));
+}
